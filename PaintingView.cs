54272676dee6ceaa1b277a88dc127c1aee964426
BaB <yo.i.jewelry.bab@gmail.com>
2014-01-03 21:19:25 +0900

    Added ShaderProgram.cs

--- PaintingView.cs
+++ PaintingView.cs
@@ -18,28 +18,25 @@ namespace Isv {
 	class PaintingView : AndroidGameView
 	{
 		int viewportWidth, viewportHeight;
-		int program;
+		//int program;
 		float [] vertices;
 
 		public PaintingView (Context context, IAttributeSet attrs) :
 			base (context, attrs)
 		{
-			Init ();
+			Initialize ();
 		}
 
 		public PaintingView (IntPtr handle, Android.Runtime.JniHandleOwnership transfer)
 			: base (handle, transfer)
 		{
-			Init ();
+			Initialize ();
 		}
 
-		void Init ()
+		private void Initialize ()
 		{
 		}
 
-		// This method is called everytime the context needs
-		// to be recreated. Use it to set any egl-specific settings
-		// prior to context creation
 		protected override void CreateFrameBuffer ()
 		{
 			GLContextVersion = GLContextVersion.Gles3_0;
@@ -70,94 +67,21 @@ namespace Isv {
 			throw new Exception ("Can't load egl, aborting");
 		}
 
-		// This gets called when the drawing surface has been created
-		// There is already a GraphicsContext and Surface at this point,
-		// following the standard OpenTK/GameWindow logic
-		//
-		// Android will only render when it refreshes the surface for
-		// the first time, so if you don't call Run, you need to hook
-		// up the Resize delegate or override the OnResize event to
-		// get the updated bounds and re-call your rendering code.
-		// This will also allow non-Run-loop code to update the screen
-		// when the device is rotated.
+
+        private ShaderProgram _simple;
+
 		protected override void OnLoad (EventArgs e)
 		{
 			// This is completely optional and only needed
 			// if you've registered delegates for OnLoad
 			base.OnLoad (e);
 
-			viewportHeight = Height; viewportWidth = Width;
+			viewportHeight = Height;
+			viewportWidth = Width;
 
-			// Vertex and fragment shaders
-			string vertexShaderSrc =  "attribute vec4 vPosition;    \n" + 
-							  "void main()                  \n" +
-							  "{                            \n" +
-							  "   gl_Position = vPosition;  \n" +
-							  "}                            \n";
-
-			string fragmentShaderSrc = "precision mediump float;\n" +
-		      					   "void main()                                  \n" +
-		      					   "{                                            \n" +
-		      					   "  gl_FragColor = vec4 (1.0, 0.0, 0.0, 1.0);  \n" +
-		      					   "}                                            \n";
-
-			int vertexShader = LoadShader (All.VertexShader, vertexShaderSrc );
-			int fragmentShader = LoadShader (All.FragmentShader, fragmentShaderSrc );
-			program = GL.CreateProgram();
-			if (program == 0)
-				throw new InvalidOperationException ("Unable to create program");
-
-			GL.AttachShader (program, vertexShader);
-			GL.AttachShader (program, fragmentShader);
-
-			GL.BindAttribLocation (program, 0, "vPosition");
-			GL.LinkProgram (program);
-
-			int linked;
-			GL.GetProgram (program, All.LinkStatus, out linked);
-			if (linked == 0) {
-				// link failed
-				int length = 0;
-				GL.GetProgram (program, All.InfoLogLength, out length);
-				if (length > 0) {
-					var log = new StringBuilder (length);
-					GL.GetProgramInfoLog (program, length, out length, log);
-					Log.Debug ("GL2", "Couldn't link program: " + log.ToString ());
-				}
-
-				GL.DeleteProgram (program);
-				throw new InvalidOperationException ("Unable to link program");
-			}
+			_simple = new ShaderProgram (Context.Assets, "Resources/shaders/Simple.vsh", "Resources/shaders/Simple.fsh"); 
 
 			RenderTriangle ();
-		}
-
-		int LoadShader (All type, string source)
-		{
-			int shader = GL.CreateShader (type);
-			if (shader == 0)
-				throw new InvalidOperationException ("Unable to create shader");
-
-			int length = 0;
-			GL.ShaderSource (shader, 1, new string [] {source}, (int[])null);
-			GL.CompileShader (shader);
-
-			int compiled = 0;
-			GL.GetShader (shader, All.CompileStatus, out compiled);
-			if (compiled == 0) {
-				length = 0;
-				GL.GetShader (shader, All.InfoLogLength, out length);
-				if (length > 0) {
-					var log = new StringBuilder (length);
-					GL.GetShaderInfoLog (shader, length, out length, log);
-					Log.Debug ("GL2", "Couldn't compile shader: " + log.ToString ());
-				}
-
-				GL.DeleteShader (shader);
-				throw new InvalidOperationException ("Unable to compile shader of type : " + type.ToString ());
-			}
-
-			return shader;
 		}
 
 		void RenderTriangle ()
@@ -172,7 +96,8 @@ namespace Isv {
 			GL.Clear (ClearBufferMask.ColorBufferBit);
 
 			GL.Viewport (0, 0, viewportWidth, viewportHeight);
-			GL.UseProgram (program);
+
+			_simple.Use ();
 
 			GL.VertexAttribPointer (0, 3, All.Float, false, 0, vertices);
 			GL.EnableVertexAttribArray (0);
---
+++ ShaderProgram.cs
@@ -0,0 +1,132 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+using Android.Content;
+using Android.Content.Res;
+using Android.Util;
+using Android.Views;
+
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES30;
+using OpenTK.Platform;
+using OpenTK.Platform.Android;
+using OpenTK;
+
+namespace Isv
+{
+	public class ShaderProgram : IDisposable
+	{
+        private int _program;
+        private int _vsh;
+        private int _fsh;
+
+		public ShaderProgram(AssetManager assetMan, string vshPath, string fshPath)
+        {
+            _program = GL.CreateProgram();
+			_vsh = LoadShader(All.VertexShader,   LoadShaderSource(assetMan, vshPath));
+			_fsh = LoadShader(All.FragmentShader, LoadShaderSource(assetMan, fshPath));
+
+            System.Diagnostics.Debug.Assert(_program != 0);
+            System.Diagnostics.Debug.Assert(_vsh != 0);
+            System.Diagnostics.Debug.Assert(_fsh != 0);
+
+			GL.AttachShader(_program, _vsh);
+			GL.AttachShader(_program, _fsh);
+
+			GL.BindAttribLocation (_program, 0, "vPosition");
+			GL.LinkProgram (_program);
+
+			int linked;
+			GL.GetProgram (_program, All.LinkStatus, out linked);
+			if (linked == 0) {
+				// link failed
+				int length = 0;
+				GL.GetProgram (_program, All.InfoLogLength, out length);
+				if (length > 0) {
+					var log = new StringBuilder (length);
+					GL.GetProgramInfoLog (_program, length, out length, log);
+					Log.Debug ("ShaderProgram", "Couldn't link program: " + log.ToString ());
+				}
+
+				GL.DeleteProgram (_program);
+                _program = 0;
+				throw new InvalidOperationException ("Unable to link program");
+			}
+        }
+
+        public void Dispose()
+        {
+            Release();
+        }
+
+        public void Use()
+        {
+            if (_program == 0)
+                return;
+
+            GL.UseProgram(_program);
+        }
+     
+        private void Release()
+        {
+            if (_vsh != 0)
+            {
+                GL.DeleteShader(_vsh);
+                _vsh = 0;
+            }
+
+            if (_fsh != 0)
+            {
+                GL.DeleteShader(_fsh);
+                _fsh = 0;
+            }
+
+            if (_program != 0)
+            {
+                GL.DeleteProgram(_program);
+                _program = 0;
+            }
+        }
+
+		private static string LoadShaderSource(AssetManager assetMan, string path)
+        {
+			using (var input = assetMan.Open(path))
+			using (var reader = new StreamReader(input))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+		private static int LoadShader (All type, string source)
+		{
+			int shader = GL.CreateShader (type);
+			if (shader == 0)
+				throw new InvalidOperationException ("Unable to create shader");
+
+			int length = 0;
+			GL.ShaderSource (shader, 1, new string [] {source}, (int[])null);
+			GL.CompileShader (shader);
+
+			int compiled = 0;
+			GL.GetShader (shader, All.CompileStatus, out compiled);
+
+			if (compiled == 0) {
+				length = 0;
+				GL.GetShader (shader, All.InfoLogLength, out length);
+				if (length > 0) {
+					var log = new StringBuilder (length);
+					GL.GetShaderInfoLog (shader, length, out length, log);
+					Log.Debug ("ShaderProgram", "Couldn't compile shader: " + log.ToString ());
+				}
+
+				GL.DeleteShader (shader);
+				throw new InvalidOperationException ("Unable to compile shader of type : " + type.ToString ());
+			}
+
+			return shader;
+		}
+	}
+}
+
